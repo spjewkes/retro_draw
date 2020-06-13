7811fe0e0028e984f96c1826482e825f39416743
Steve Jewkes <steve@mulus.net>
2020-06-13 18:18:11 +0100

    Added sliders to change opacity of guide image and grid

--- retro_draw.py
+++ retro_draw.py
@@ -3,7 +3,7 @@
 import sys
 from enum import Enum
 from PySide2.QtWidgets import QApplication, QDialog, QLineEdit, QPushButton, QVBoxLayout, QWidget, QHBoxLayout, \
-    QLabel, QCheckBox, QButtonGroup, QGroupBox, QFileDialog
+    QLabel, QCheckBox, QButtonGroup, QGroupBox, QFileDialog, QSlider
 from PySide2.QtGui import QIcon, QPainter, QBrush, QPen, QColor, QFont, QImage, QPixmap
 from PySide2.QtCore import QSize, QRect, QPoint, Qt, Slot
 from retmod.zxbuffer import ZXSpectrumBuffer, ZXAttribute
@@ -27,7 +27,6 @@ class RetroDrawWidget(QWidget):
 
         self.scale = 4
         self.screenSize = self.canvasSize * self.scale
-        self.guideOpacity = 0.2
 
         self.grid = QImage(self.screenSize, QImage.Format_RGBA8888)
         self.grid.fill(QColor(0, 0, 0, 0))
@@ -39,9 +38,11 @@ class RetroDrawWidget(QWidget):
             for y in range(0, self.screenSize.height(), 8 * self.scale):
                 self.grid.setPixelColor(x, y, QColor(0, 0, 0, 255))
         self._gridEnabled = True
+        self._gridOpacity = 0.2
 
         self._guide = None
         self._guideEnabled = True
+        self._guideOpacity = 0.2
         
         self.drawable = ZXSpectrumBuffer()
 
@@ -65,10 +66,11 @@ class RetroDrawWidget(QWidget):
         rectSource = QRect(QPoint(0, 0), self.canvasSize)
         painter.drawPixmap(rectTarget, self.drawable.qpixmap, rectSource)
 
-        painter.setOpacity(self.guideOpacity)
         if self._guide and self._guideEnabled:
-             painter.drawPixmap(rectTarget, self._guide, self._guide.rect())
+            painter.setOpacity(self._guideOpacity)
+            painter.drawPixmap(rectTarget, self._guide, self._guide.rect())
         if self._gridEnabled:
+            painter.setOpacity(self._gridOpacity)
             painter.drawImage(rectTarget, self.grid, rectTarget)
 
         painter.end()
@@ -114,16 +116,24 @@ class RetroDrawWidget(QWidget):
     def saveImage(self, filename, format=None):
         self.drawable.saveBuffer(filename)
         
-    def setGuideImage(self, filename):
-        self._guide = QPixmap(filename)
-        self.repaint()
-        
     def setGrid(self, checked):
         self._gridEnabled = checked
         self.repaint()
         
+    def setGridOpacity(self, value):
+        self._gridOpacity = value / 100.0
+        self.repaint()
+        
+    def setGuideImage(self, filename):
+        self._guide = QPixmap(filename)
+        self.repaint()
+        
     def setGuide(self, checked):
         self._guideEnabled = checked
+        self.repaint()
+
+    def setGuideOpacity(self, value):
+        self._guideOpacity = value / 100.0
         self.repaint()
 
 class Form(QDialog):
@@ -158,9 +168,32 @@ class Form(QDialog):
         enable_grid_check.clicked.connect(self._setGrid)
         self._retroWidget.setGrid(True)
         buttons.addWidget(enable_grid_check)
+        
+        sliders = QHBoxLayout()
+        # Guide slider
+        sliders.addWidget(QLabel("Guide Opacity:"))
+        guide_slider = QSlider(Qt.Horizontal)
+        guide_slider.setTickInterval(10)
+        guide_slider.setTickPosition(QSlider.TicksBothSides)
+        guide_slider.setSingleStep(1)
+        guide_slider.valueChanged.connect(self._setGuideSlider)
+        guide_slider.setValue(20)
+        self._retroWidget.setGuideOpacity(20)
+        sliders.addWidget(guide_slider)
+        # Grid slider
+        sliders.addWidget(QLabel("Grid Opacity:"))
+        grid_slider = QSlider(Qt.Horizontal)
+        grid_slider.setTickInterval(10)
+        grid_slider.setTickPosition(QSlider.TicksBothSides)
+        grid_slider.setSingleStep(1)
+        grid_slider.valueChanged.connect(self._setGridSlider)
+        grid_slider.setValue(20)
+        self._retroWidget.setGridOpacity(20)
+        sliders.addWidget(grid_slider)
 
         layout = QVBoxLayout()
         layout.addLayout(buttons)
+        layout.addLayout(sliders)
         layout.addSpacing(10)
         layout.addWidget(self._paletteWidget)
         layout.addSpacing(10)
@@ -174,18 +207,26 @@ class Form(QDialog):
         self._retroWidget.saveImage("output.png")
         
     @Slot()
+    def _setGrid(self, checked):
+        self._retroWidget.setGrid(checked)
+        
+    @Slot()
+    def _setGridSlider(self, value):
+        self._retroWidget.setGridOpacity(value)
+
+    @Slot()
     def _setGuideImage(self):
         filename = QFileDialog.getOpenFileName(self, "Choose guide image", ".", "Image Files (*.png *.jpg *.bmp)")
         if filename[0]:
             self._retroWidget.setGuideImage(filename[0])
             
     @Slot()
-    def _setGrid(self, checked):
-        self._retroWidget.setGrid(checked)
-        
-    @Slot()
     def _setGuide(self, checked):
         self._retroWidget.setGuide(checked)
+        
+    @Slot()
+    def _setGuideSlider(self, value):
+        self._retroWidget.setGuideOpacity(value)
 
 if __name__ == "__main__":
     # Create the Qt Application
